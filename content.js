78059fc3798f74614ff03eb12647bce832bb8ce7
Gabriel Senna <usasenna@gmail.com>
2022-06-07 17:04:52 -0300

    funcionando em parte

--- content.js
+++ content.js
@@ -1,13 +1,20 @@
 
+// Notification.requestPermission(/* opcional: callback */);
+
+
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 function lerData(data) {
-    var dia;
-    var mes;
-    var ano;
+    var dia = data;
+    var mes = data;
+    var ano = data;
 
     // dia = 2 primeiros digitos
     // mes = 4° digito até a virgula
     // ano = ultimos 4 digitos
-    
+
     if(mes = "Janeiro"){mes = 1}
     if(mes = "Fevereiro"){mes = 2}
     if(mes = "Março"){mes = 3}
@@ -25,9 +32,7 @@ function lerData(data) {
 }
 
 
-setInterval(() => {
-    location.reload();
-
+setInterval(async() => {
     var url_atual = window.location.href;
     var url_login = 'https://ais.usvisa-info.com/pt-br/niv/users/sign_in';
     var url_pay = 'https://ais.usvisa-info.com/pt-br/niv/schedule/39894926/payment';
@@ -35,7 +40,13 @@ setInterval(() => {
 
     console.log("url:", url_atual)
 
+    
+
     if(url_atual == url_pay){
+        // location.reload();
+        var email = window.localStorage.getItem('email');
+        var senha =  window.localStorage.getItem('senha');
+
         var proximaData = document.querySelector('tbody .text-right').innerText;
         var proximaDataBsb = window.localStorage.getItem('proximaDataBsb');
         console.log("proximaDataBsb:", proximaDataBsb )
@@ -43,6 +54,12 @@ setInterval(() => {
         console.log("if pagina pay");
         console.log(proximaData);
         window.localStorage.setItem('proximaDataBsb', proximaData);
+
+        var texto = "próxima data: " + proximaData;
+
+        var notification2 = new Notification(texto, {
+            body: "essa é a proxima data definida pelo Consul em Brasília."
+        });
 
         if(proximaDataBsb =! proximaData){
             console.log("mudou alguma coisa")
@@ -55,17 +72,32 @@ setInterval(() => {
     else{    
         if(url_atual == url_login){
 
+
+            if(window.localStorage.getItem('email') == null || window.localStorage.getItem('password') == null) {
+                let email = prompt('Digite seu email: ');
+                window.localStorage.setItem('email', email);
+
+                let password = prompt('Digite sua senha: ');
+                window.localStorage.setItem('password', password);
+            }
+
+            console.log('Email: ' + localStorage.getItem('email'));
+            console.log('Senha: ' + localStorage.getItem('password'));
+
             console.log("if pagina login")
             //verifica se ta preenchido, se n tiver da alerta de erro
+
+            document.getElementById('user_email').value = localStorage.getItem('email');
+            document.getElementById('user_password').value = localStorage.getItem('password');
+
             let setID = document.getElementById('policy_confirmed');
             setID.checked = true;
 
             let button = document.querySelector('.button');
 
-            setTimeout( () => {
-                button.click();
-                console.log("clico")
-            } ,2000)
+            button.click();
+            await sleep(300);
+            console.log("clico")
         }
         else{
 
@@ -73,4 +105,4 @@ setInterval(() => {
             window.location.href = url_pay; 
         }}
 
-}, 31000);
+}, 4500);
