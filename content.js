69463bfc56c957fded856668df169f1fed73f4dd
Gabriel Senna <usasenna@gmail.com>
2022-06-08 16:56:41 -0300

    tela de agendamento

--- content.js
+++ content.js
@@ -9,16 +9,15 @@ setInterval(async() => {
     var url_atual = window.location.href;
     var url_login = 'https://ais.usvisa-info.com/pt-br/niv/users/sign_in';
     var url_pay = 'https://ais.usvisa-info.com/pt-br/niv/schedule/39962141/payment';
-    var best_find;
+    var url_agenda = 'https://ais.usvisa-info.com/pt-br/niv/schedule/39894926/appointment';
+
+    // await window.location.reload();
+
+    
 
     // console.log("url:", url_atual)
 
     if(url_atual == url_pay){
-        location.reload();
-        await sleep(1000);
-
-        var email = window.localStorage.getItem('email');
-        var senha =  window.localStorage.getItem('senha');
 
         var proximaData = document.querySelector('tbody .text-right').innerText;
         var ultimaDataVista = window.localStorage.getItem('ultimaDataVista');
@@ -29,35 +28,70 @@ setInterval(async() => {
 
         window.localStorage.setItem('ultimaDataVista', proximaData);
 
-        var compara = comparaData(proximaData);
+        var compara = comparaDataPortugues(proximaData);
         if(compara == '1'){
             console.log("ta comparando")
             var notification = new Notification(("data melhor: " + proximaData), {
                 body: ("essa é a proxima data definida em Brasília.", proximaData)
             });
-            var melhordata = melhorData(proximaData);
-            if(melhordata == '1'){
-                var aux = lerData(proximaData);
+            var melhordataPortugues = melhorDataPortugues(proximaData);
+            if(melhordataPortugues == '1'){
+                var aux = lerDataPortugues(proximaData);
                 window.localStorage.setItem('melhorDia', aux[0]);
                 window.localStorage.setItem('melhorMes', aux[1]);
                 window.localStorage.setItem('melhorAno', aux[2]);
             }
         }
-
-        // console.log("dia: ", window.localStorage.getItem('lerDia'));
-        // console.log("mes: ", window.localStorage.getItem('lerMes'));
-        // console.log("ano: ", window.localStorage.getItem('lerAno'));
         console.log("melhor data até agora:",window.localStorage.getItem('melhorDia'),window.localStorage.getItem('melhorMes'),window.localStorage.getItem('melhorAno'))
         
-        // pega proximaData
-        // if proximaData < dataMarcada
-        // salva no historico e dá um alerta!
     }
 
-    else{    
-        if(url_atual == url_login){
-            console.log("Estamos na página de login")
+    else if(url_atual == url_agenda){
+        console.log("debug1")
+        var proximoDia = null ;
+        var proximoMes ;
+        var proximoAno ;
 
+
+        var carregarMais = document.getElementsByClassName('ui-datepicker-next');
+        var calendario = document.querySelector('#appointments_consulate_appointment_date');
+        
+        calendario.click();
+
+        console.log("ants while")
+
+        while(proximoDia == null){
+
+            console.log("depois while")
+
+            try {
+                proximoDia = document.querySelector('tbody tr td a').innerText;
+            } catch(error){
+                carregarMais[0].click();
+            }
+        }
+
+        try {
+            proximoDia = document.querySelector('tbody tr td a');
+            proximoDia = document.querySelector('tbody tr td a').innerText;
+            proximoMes = document.querySelector('tbody tr td a').parentNode.attributes[3].value;
+            proximoAno = document.querySelector('tbody tr td a').parentNode.attributes[4].value;
+            
+        } catch (error) {
+            console.log("deu erro" )
+        }
+
+        console.log("dentro do if proximoDIa null:", proximoDia )
+
+        console.log("proxima data: ", proximoDia);
+        console.log("proximo mes: ", proximoMes);
+        console.log("proximo ano: ", proximoAno);
+
+    }
+
+    else if(url_atual == url_login){
+
+            console.log("Estamos na página de login")
 
             if(window.localStorage.getItem('email') == null || window.localStorage.getItem('password') == null) {
                 let email = prompt('Digite seu email: ');
@@ -84,17 +118,17 @@ setInterval(async() => {
             console.log("cliquei em fazer login");
             await sleep(300);
         }
-        else{
 
+    else{
             console.log("esta página é estranha, vou redirecionar para a página de pagamento")
-            window.location.href = url_pay; 
-        }}
+            window.location.href = url_agenda; 
+        }
         console.log(' ');
 
-}, 31000);
+}, 4000);
 
 
-function lerData(data) {
+function lerDataPortugues(data) {
     var dia = data[0]+data[1];
     var ano = data[data.length-4]+data[data.length-3]+data[data.length-2]+data[data.length-1];
     var mes;
@@ -137,11 +171,11 @@ function lerData(data) {
     return [dia, mes, ano]
 }
 
-function comparaData(data){
+function comparaDataPortugues(data){
     // return 0 para data ruim
     // return 1 para data boa
 
-    let proximaData = lerData(data);
+    let proximaData = lerDataPortugues(data);
     var dia = proximaData[0];
     var mes = proximaData[1];
     var ano = proximaData[2];
@@ -172,14 +206,73 @@ function comparaData(data){
     }
 }
 
-function melhorData(data){
+function melhorDataPortugues(data){
     // return 0 para data ruim
     // return 1 para data boa
 
-    let proximaData = lerData(data);
+    let proximaData = lerDataPortugues(data);
     var dia = proximaData[0];
     var mes = proximaData[1];
     var ano = proximaData[2];
+    var melhorDia = window.localStorage.getItem('melhorDia');
+    var melhorMes = window.localStorage.getItem('melhorMes');
+    var melhorAno = window.localStorage.getItem('melhorAno');
+    
+    console.log("dia: ", dia, "mes: ", mes, "ano", ano);
+
+    if(melhorAno > ano){
+        console.log("melhor data por ano")
+        return '1'
+    }
+    else if(melhorAno == ano && melhorMes > mes){
+        console.log("melhor data por mes")
+        return '1'
+    }
+    else if(melhorAno == ano && melhorMes == mes && melhorDia > dia){
+        console.log("melhor data por dia")
+        return '1'
+    }
+    else{
+        console.log("n é a melhor data")
+        return '0'
+    }
+}
+
+function comparaDataCalendario(dia, mes, ano){
+    // return 0 para data ruim
+    // return 1 para data boa
+
+    var diaMarcado = window.localStorage.getItem('diaMarcado');
+    var mesMarcado = window.localStorage.getItem('mesMarcado');
+    var anoMarcado = window.localStorage.getItem('anoMarcado');
+    
+
+    if(anoMarcado < ano){
+        console.log("Ano superior")
+        return '0'
+    }
+    else if(anoMarcado == ano && mesMarcado < mes){
+        console.log("mes superior")
+        return '0'
+    }
+    else if(anoMarcado == ano && mesMarcado == mes && diaMarcado < dia){
+        console.log("dia superior")
+        return '0'
+    }
+    else if(anoMarcado == ano && mesMarcado == mes && diaMarcado == dia){
+        console.log("dia igual")
+        return '0'
+    }
+    else{
+        console.log("data mais próxima")
+        return '1'
+    }
+}
+
+function melhorDataCalendario(dia, mes, ano){
+    // return 0 para data ruim
+    // return 1 para data boa
+
     var melhorDia = window.localStorage.getItem('melhorDia');
     var melhorMes = window.localStorage.getItem('melhorMes');
     var melhorAno = window.localStorage.getItem('melhorAno');
